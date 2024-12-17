90809582e4ed2048de6a9f2965f6bc26c720d14f
zhoumaoling <zhoumaoling@baidu.com>
2024-12-18 00:29:20 +0800

    删除无用文件

--- src/pages/me/modify.vue
+++ src/pages/me/modify.vue
@@ -26,39 +26,6 @@
 				<uni-icons type="compose" size="30" color="#D3D3D3"></uni-icons>
 			</template>
 		</one-row-card>
-
-		<one-row-card>
-			<template #left>性别</template>
-			<template #rightText>{{ user.gender || userDefaultData.gender }}</template>
-			<template #rightIcon>
-				<picker mode="selector" :range="genderRange" :value="user.gender" @change="bindGenderChange">
-					<uni-icons type="forward" size="30" color="#D3D3D3"></uni-icons>
-				</picker>
-			</template>
-		</one-row-card>
-
-		<one-row-card>
-			<template #left>城市</template>
-			<template #rightText>{{ user.province || userDefaultData.province }}</template>
-			<template #rightIcon>
-				<picker mode="selector" :range="provinceRange" :value="user.province" @change="bindProvinceChange">
-					<uni-icons type="forward" size="30" color="#D3D3D3"></uni-icons>
-				</picker>
-			</template>
-		</one-row-card>
-
-		<one-row-card>
-			<template #left>生日</template>
-			<template #rightText>
-				<view>{{ user.birthday || userDefaultData.birthday }}</view>
-			</template>
-			<template #rightIcon>
-				<picker mode="date" :value="user.birthday" @change="bindDateChange">
-					<uni-icons type="forward" size="30" color="#D3D3D3"></uni-icons>
-				</picker>
-			</template>
-		</one-row-card>
-
 		<view class="save" @click="save"> 保存修改 </view>
 		<view class="logout" @click="logout"> 退出登录 </view>
 	</view>
@@ -67,90 +34,52 @@
 import oneRowCard from "@/components/common/oneRowCard.vue";
 import { reactive } from "vue";
 import { useAuthStore } from "@/stores/auth";
-import { ossHost, provincesInChina, userDefaultData } from "@/const";
-import { uniUploadFile } from "@/api/uni.api";
+import { ossHost, userDefaultData } from "@/const";
+import { uniUploadFile } from "@/api/uni.js";
 import { isValidKey } from "@/utils/tools";
 import { getStringWidth } from "@/utils/tools";
 import UniIcons from '@/common/uni-icons/uni-icons.vue';
-
+const HOST_URL = "http://127.0.0.1:7001"
 const meStore = useAuthStore();
-
-// 模拟用户数据（mock 数据）
-const mockUserData = {
-  avatarUrl: "https://example.com/avatar.jpg",
-  nickName: "小明",
-  gender: "男",
-  province: "广东省",
-  birthday: "2000-01-01",
-};
-
-const user = reactive({
-  avatarUrl: mockUserData.avatarUrl,
-  nickName: mockUserData.nickName,
-  gender: mockUserData.gender,
-  province: mockUserData.province,
-  birthday: mockUserData.birthday,
-});
-
-const genderRange = ["男", "女", "其他"];
-const provinceRange = provincesInChina;
+const user = reactive({});
 
 function onChooseAvatar(e) {
-  console.log("choose avatar: ", e.detail);
   user.avatarUrl = e.detail.avatarUrl;
-}
-
-function bindGenderChange(e) {
-  user.gender = genderRange[e.detail.value];
 }
 
 function handleNickNameChange(e) {
   user.nickName = e.detail.value;
 }
 
-function bindProvinceChange(e) {
-  user.province = provinceRange[e.detail.value];
-}
-
-function bindDateChange(e) {
-  user.birthday = e.detail.value;
-}
-
 async function save() {
   const len = getStringWidth(user.nickName);
-  console.log("nickName len: ", len);
   
   if (len > 10 || len === 0) {
     uni.showToast({
-      title: "昵称长度错误",
+      title: "昵称过长，请修改",
       icon: "error",
       duration: 2000,
     });
     throw new Error("昵称长度错误");
   }
 
-  if (mockUserData.avatarUrl !== user.avatarUrl) {
-    // 模拟头像上传（返回静态的头像 URL）
-    user.avatarUrl = await uploadAvatar(user.avatarUrl);
-  }
+  // if (meStore.user?.avatarUrl !== user.avatarUrl) {
+  //   // 模拟头像上传（返回静态的头像 URL）
+  //   user.avatarUrl = await uploadAvatar(user.avatarUrl);
+  // }
 
   // 模拟用户更新参数
-  const userParams = {};
-  Object.entries(user).forEach(([key, val]) => {
-    if (isValidKey(key, mockUserData)) {
-      if (mockUserData[key] !== val) {
-        userParams[key] = val;
-      }
-    }
-  });
+  // const userParams = {};
+  // Object.entries(user).forEach(([key, val]) => {
+  //   if (isValidKey(key, mockUserData)) {
+  //     if (mockUserData[key] !== val) {
+  //       userParams[key] = val;
+  //     }
+  //   }
+  // });
 
-  console.log("userParams: ", userParams);
 
-  // 模拟更新用户信息（无需调用 GraphQL）
-  console.log("User information updated:", userParams);
-
-  // 更新 Pinia store（模拟更新）
-  meStore.$patch({ user: { ...mockUserData, ...user } });
+  meStore.$patch({ user });
   uni.navigateBack({
     delta: 1,
   });
